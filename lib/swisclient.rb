e4b59d62c14bd1654ce1389f9e78f4f4e25cc168
Nathan Powell <nathan@nathanpowell.org>
2015-08-10 19:44:13 -0400

    Making a raw query work.  still lots to do

--- lib/swisclient.rb
+++ lib/swisclient.rb
@@ -1,6 +1,6 @@
 require "uri"
-require "yaml"
 require "json"
+require "openssl"
 require "net/http"
 
 class Swisclient
@@ -10,22 +10,39 @@ class Swisclient
     @port = port
     @username = username
     @password = password
+    @querypath = "/SolarWinds/InformationService/v3/Json/Query"
+    @crudpath = ""
+    @initheader = {'Content-Type' =>'application/json'}
+    @usessl = true
   end
 
+  # This is a raw query function, no syntactical sugar
+  # The query should be a ruby hash similar to:
+  # {"query" => "SELECT NodeID FROM Orion.Nodes WHERE NodeName=@name", "parameters" => {"name" => "myhost.mydomain.com"}}
   def query(query)
+    http_client, uri = build_http_request(@querypath)
+    request = Net::HTTP::Post.new(uri.request_uri, initheader = @initheader)
+    request.body = query.to_json
+    request.basic_auth(@username.to_s, @password.to_s)
+    http_client.request(request).body.to_s
   end
 
+  def build_http_request(path)
+    uri = URI.parse(@host + ":" + @port + "#{path}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = @usessl
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    return http, uri
+  end
+
+  # Methods for typical operations
   def query_by_nodename(name)
   end
 
   def query_by_nodeid(nodeid)
   end
-
+  
   def query_by_ipaddress(ipaddress)
-  end
-
-  def build_http_request()
-    
   end
 
 end
